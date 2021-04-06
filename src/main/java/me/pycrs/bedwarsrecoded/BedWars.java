967715fb34c2c08defee392f4d09c7f6ccabaf3d
Pycrs <petrbohac3@seznam.cz>
2021-04-06 09:21:26 +0200

    Initial commit

---
+++ src/main/java/me/pycrs/bedwarsrecoded/BedWars.java
@@ -0,0 +1,28 @@
+package me.pycrs.bedwarsrecoded;
+
+import me.pycrs.bedwarsrecoded.listeners.AsyncChatListener;
+import me.pycrs.bedwarsrecoded.listeners.PlayerJoinListener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BedWars extends JavaPlugin {
+    private static boolean gameInProgress = false;
+
+    @Override
+    public void onEnable() {
+        init();
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+    private void init() {
+        new PlayerJoinListener(this);
+        new AsyncChatListener(this);
+    }
+
+    public static boolean isGameInProgress() {
+        return gameInProgress;
+    }
+}
