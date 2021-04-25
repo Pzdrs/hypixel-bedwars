de90cef0b6ebaadf3b81fa1db2f6e0951d29ec02
Pycrs <petrbohac3@seznam.cz>
2021-04-25 14:07:07 +0200

    Event was getting called twice cuz u have two hands in this version so yeah, fixed that

--- src/main/java/me/pycrs/bedwarsrecoded/listeners/PlayerInteractEntityListener.java
+++ src/main/java/me/pycrs/bedwarsrecoded/listeners/PlayerInteractEntityListener.java
@@ -7,6 +7,7 @@ import org.bukkit.entity.Entity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.inventory.EquipmentSlot;
 
 public class PlayerInteractEntityListener implements Listener {
     private BedWars plugin;
@@ -20,13 +21,15 @@ public class PlayerInteractEntityListener implements Listener {
     public void onEntityClick(PlayerInteractEntityEvent event) {
         // TODO: 4/11/2021 check if entity is an actual shopkeeper, probably gonna use persistent data container
         Entity entity = event.getRightClicked();
-        switch (entity.getType()) {
-            case VILLAGER:
-                new GenericShop(event.getPlayer()).open();
-                break;
-            case SHEEP:
-                new TeamUpgradesShop(event.getPlayer()).open();
-                break;
+        if (event.getHand().equals(EquipmentSlot.HAND)) {
+            switch (entity.getType()) {
+                case VILLAGER:
+                    new GenericShop(event.getPlayer()).open();
+                    break;
+                case SHEEP:
+                    new TeamUpgradesShop(event.getPlayer()).open();
+                    break;
+            }
         }
     }
 }
