26bddbd06f0e21635216077f095ca152846cebdd
Petr <petrbohac3@seznam.cz>
2021-06-13 13:02:13 +0200

    Added a respawn message

--- src/main/java/me/pycrs/bedwarsrecoded/listeners/BWPlayerRespawnListener.java
+++ src/main/java/me/pycrs/bedwarsrecoded/listeners/BWPlayerRespawnListener.java
@@ -1,6 +1,7 @@
 package me.pycrs.bedwarsrecoded.listeners;
 
 import me.pycrs.bedwarsrecoded.BedWars;
+import me.pycrs.bedwarsrecoded.Utils;
 import me.pycrs.bedwarsrecoded.events.BWPlayerDeathEvent;
 import me.pycrs.bedwarsrecoded.events.BWPlayerRespawnEvent;
 import net.kyori.adventure.text.Component;
@@ -26,6 +27,7 @@ public class BWPlayerRespawnListener implements Listener {
         event.getPlayer().setSpectator(false);
         Player player = event.getPlayer().getPlayer();
         player.setInvisible(false);
+        player.sendMessage(Component.text("You have respawned!", NamedTextColor.YELLOW));
         player.showTitle(Title.title(Component.text("RESPAWNED!", NamedTextColor.GREEN), Component.empty(),
                 Title.Times.of(Duration.ZERO, Duration.ofMillis(1500), Duration.ZERO)));
     }
