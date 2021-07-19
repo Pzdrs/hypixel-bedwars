33021370469dbb8f2b064eb1b7cceb40cbeea3fa
Petr <petrbohac3@seznam.cz>
2021-07-19 14:22:08 +0200

    Added display name property to modes to be later displayed in a scoreboard

--- src/main/java/me/pycrs/bedwars/Mode.java
+++ src/main/java/me/pycrs/bedwars/Mode.java
@@ -1,17 +1,23 @@
 package me.pycrs.bedwars;
 
 public enum Mode {
-    SOLO(1, 8, 2),
-    DOUBLES(2, 8,4),
-    TRIOS(3, 4, 6),
-    SQUADS(4, 4, 8);
+    SOLO(1, 8, 2, "Solo"),
+    DOUBLES(2, 8,4, "Doubles"),
+    TRIOS(3, 4, 6, "3v3v3v3"),
+    SQUADS(4, 4, 8, "4v4v4v4");
 
     private final int teamSize, amountOfTeams, minPlayers;
+    private final String displayName;
 
-    Mode(int teamSize, int amountOfTeams, int minPlayers) {
+    Mode(int teamSize, int amountOfTeams, int minPlayers, String displayName) {
         this.teamSize = teamSize;
         this.amountOfTeams = amountOfTeams;
         this.minPlayers = minPlayers;
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
     }
 
     public int getMinPlayers() {
